309096ce93276823cac42361804c2e63f3f08621
Rubén Romero Planas <rromero@cifpfbmoll.eu>
2020-10-16 20:55:03 +0200

    Funcionando

--- src/matrix_effect/Matrix_Effect.java
+++ src/matrix_effect/Matrix_Effect.java
@@ -16,17 +16,35 @@ public class Matrix_Effect {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int option = 1;
 
-        int matrix_alter[][] = new_alter();
-        int matrix_3d[][][] = random_matrix(5, 5, 2);
+        if (option == 1) {
+            int vector_alter[] = vector_alter();
 
-        int new_matrix[][][] = new_matrix(matrix_3d, matrix_alter);
+            int vector[] = random_vector(5, 5, 1);
 
-        System.out.println("Matriz original: ");
-        show_matrix(matrix_3d);
+            int new_vector[] = new_vector(vector, vector_alter);
 
-        System.out.println("Matriz nueva: ");
-        show_matrix(new_matrix);
+            System.out.println("Vector de original: " + vector.length);
+            show_vector(vector);
+
+            System.out.println("Vector de nuevo: " + new_vector.length);
+            show_vector(new_vector);
+
+        } else {
+            int matrix_alter[][] = matrix_alter();
+
+            int matrix_3d[][][] = random_matrix(5, 5, 2);
+
+            int new_matrix[][][] = new_matrix(matrix_3d, matrix_alter);
+
+            System.out.println("Matriz original: ");
+            show_matrix(matrix_3d);
+
+            System.out.println("Matriz nueva: ");
+            show_matrix(new_matrix);
+        }
+
     }
 
     public static int[][][] random_matrix(int col, int row, int prof) {
@@ -66,7 +84,7 @@ public class Matrix_Effect {
         return matrix_3d;
     }
 
-    public static int[][] new_alter() {
+    public static int[][] matrix_alter() {
 
         int matrix_alter[][] = {
             {1, 1, 1},
@@ -137,7 +155,62 @@ public class Matrix_Effect {
                 k_sum += matrix_alter[i][j];
             }
         }
-        
+
         return k_sum;
+    }
+
+    public static int[] random_vector(int col, int row, int prof) {
+
+        int vector[] = new int[col * row * prof];
+
+        for (int i = 0; i < (col * row * prof); i++) {
+            vector[i] = (int) (Math.random() * 9) + 1;
+        }
+        return vector;
+    }
+
+    public static int[] show_vector(int[] vector) {
+
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + " ");
+        }
+
+        System.out.println("\n");
+
+        return vector;
+    }
+
+    public static int[] vector_alter() {
+
+        int vector_alter[] = {1, 1, 1, 0, 2, 0, 1, 1, 1};
+
+        for (int i = 0; i < 9; i++) {
+            System.out.print(vector_alter[i] + " ");
+        }
+
+        System.out.println("");
+
+        return vector_alter;
+    }
+
+    public static int[] new_vector(int[] vector, int[] vector_alter) {
+
+        int new_vector[] = new int[vector.length + vector_alter.length - 1];
+
+        for (int i = 0; i < vector.length  + vector_alter.length - 1; i++) {
+            int sum_p = 0;
+
+            for (int j = 0; j < vector_alter.length; j++) {
+
+                int pos = i + j - (vector_alter.length - 1);
+
+                if (pos >= 0 && pos < vector.length) {
+                        sum_p += (vector[pos] * vector_alter[j]);
+                }
+            }
+
+            new_vector[i] = sum_p;
+        }
+        return new_vector;
     }
 }
